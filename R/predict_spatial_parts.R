53be7e2674787fe8f471075f9b342c97441fae54
Jakub Nowosad <tupiszakaczki@gmail.com>
2024-11-11 16:55:16 +0100

    updates code and adds docs

--- R/predict_spatial_parts.R
+++ R/predict_spatial_parts.R
@@ -1,13 +1,15 @@
-#' Title
+#' Predict spatial parts
 #'
-#' @param explainer
+#' This function calculates the attributions of the model for each observation in the raster.
+#'
+#' @param explainer a model to be explained, preprocessed by the [`DALEX::explain()`] function
 #' @param raster_obs
 #' @param maxcell
-#' @param ...
-#' @param N
-#' @param type
+#' @param ... other parameters that will be passed to [`iBreakDown::break_down()`]
+#' @param N the maximum number of observations used for calculation of attributions. By default NULL (use all) or 500 (for oscillations)
+#' @param type the type of variable attributions. Either `shap`, `oscillations`, `oscillations_uni`, `oscillations_emp`, `break_down` or `break_down_interactions`
 #'
-#' @return
+#' @return A raster object with the same dimensions as `raster_obs`. The number of layers equal to the number of variables in the model or the number of variables in the model plus one if `type` is not `shap`.
 #' @export
 #'
 #' @examples
@@ -26,7 +28,7 @@ predict_spatial_parts = function(explainer, raster_obs, maxcell = 1000, ...,
   }
   for (i in seq_len(nrow(x_df))){
     if (stats::complete.cases(x_df[i, ])){
-      pp = DALEX::predict_parts(explainer, new_observation = x_df[i, ], type = "break_down_interactions")
+      pp = DALEX::predict_parts(explainer, new_observation = x_df[i, ], type = type)
       if (type == "shap"){
         pp_mean_contribution = tapply(pp$contribution, pp$variable, mean, na.rm = TRUE)
         pp_df = data.frame(contribution = pp_mean_contribution,
@@ -53,6 +55,6 @@ predict_spatial_parts = function(explainer, raster_obs, maxcell = 1000, ...,
   r_result = terra::rast(raster_obs, nlyrs = ncol(result))
   terra::values(r_result) = result
   names(r_result) = names(result)
-  plot(r_result)
-    return(r_result)
+  # plot(r_result)
+  return(r_result)
 }
