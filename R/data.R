c1da58b9f0fca2ee6ea3335dc7bd34fd63fcea6e
Jakub Nowosad <tupiszakaczki@gmail.com>
2024-11-19 13:53:10 +0100

    updates docs

--- R/data.R
+++ R/data.R
@@ -7,3 +7,12 @@
 #' @usage data(regr_exp)
 #'
 "regr_exp"
+
+#' An example of a classification model explainer
+#'
+#' An example of a model explainer (created using the DALEX package) for a classification problem.
+#'
+#' @format A list containing the model and related information.
+#'
+#' @usage data(clas_exp)
+"clas_exp"
